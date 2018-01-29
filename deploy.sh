394a45e8fa6fb73dede56749818f940743b97975
Simon Rapin <simon.rapin@gmail.com>
2018-01-29 13:52:46 +0100

    - fixed : deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo "Start npm install"
-npm install
+/opt/plesk/node/8/bin/npm install --scripts-prepend-node-path
 echo "End npm install"
 echo "Start npm run build"
-npm run build
+/opt/plesk/node/8/bin/npm run build --scripts-prepend-node-path
 echo "End npm run build"
