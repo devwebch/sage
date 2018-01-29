de419b38d3bab93388052dfc722129e933fa2af5
Simon Rapin <simon.rapin@gmail.com>
2018-01-29 14:48:37 +0100

    - fixed : deploy script

--- templates/header.php
+++ templates/header.php
@@ -13,7 +13,7 @@
                 wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
             endif;
             ?>
-            <p>Te</p>
+            <p>T</p>
         </nav>
 
         <div class="mobile-hamburger">
