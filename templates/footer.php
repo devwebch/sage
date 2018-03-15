2a62a810d4dd83b76b77ff6fea7abb8cea6b73db
Simon <simon.rapin@gmail.com>
2018-03-15 13:39:07 +0100

    -added: sub footer with copyright and credits

--- templates/footer.php
+++ templates/footer.php
@@ -13,3 +13,12 @@
         </div>
     </div>
 </footer>
+<div class="sub-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                &copy; <?php echo date('Y') . ' ' . get_bloginfo('blogname'); ?> - Créé par <a href="https://developpeurweb.ch" title="Agence Web Vaud" target="_blank" rel="noopener">DeveloppeurWeb</a>
+            </div>
+        </div>
+    </div>
+</div>
