8c5c7351683847a36f104534dff4c5498ff9bc2f
Simon Rapin <simon.rapin@gmail.com>
2018-05-31 11:09:09 +0200

    -updated: deploy script now uses node 9

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo "Start npm install"
-/opt/plesk/node/8/bin/npm install --scripts-prepend-node-path
+/opt/plesk/node/9/bin/npm install --scripts-prepend-node-path
 echo "End npm install"
 echo "Start npm run build"
-/opt/plesk/node/8/bin/npm run build --scripts-prepend-node-path
+/opt/plesk/node/9/bin/npm run build --scripts-prepend-node-path
 echo "End npm run build"
