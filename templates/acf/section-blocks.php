258adc90e9ac1fe87f8bd9d9ff994c57c9d2c003
Simon <simon.rapin@gmail.com>
2018-08-20 18:52:51 +0200

    -updated: section blocks links

--- templates/acf/section-blocks.php
+++ templates/acf/section-blocks.php
@@ -27,7 +27,9 @@ $link_label     = get_sub_field('link_label');
                                     <div class="icon-block__content">
                                         <h3><?php the_sub_field('title') ?></h3>
                                         <p><?php the_sub_field('content') ?></p>
-                                        <a href="<?php the_sub_field('link'); ?>" class="">En savoir plus</a>
+                                        <?php if (get_sub_field('link')): ?>
+                                            <a href="<?php the_sub_field('link'); ?>" class="">En savoir plus</a>
+                                        <?php endif; ?>
                                     </div>
                                 </div>
                             </div>
